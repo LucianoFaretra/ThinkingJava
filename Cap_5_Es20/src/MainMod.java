03db25ad464dc7a2984a3d4eca19356319e5661c
LucianoFaretra <lucianofaretra@gmail.com>
2017-03-02 16:01:17 +0100

    Add Es20 cap5

---
+++ Cap_5_Es20/src/MainMod.java
@@ -0,0 +1,11 @@
+/**
+ * Test main con accettazione di input al momento dell'avvio, inserire come parametro di avvio un numero arbitrario di
+ * stringhe, verranno stampate a video
+ */
+public class MainMod {
+    public static void main(String[] args){
+        for(String i : args){
+            System.out.println(i);
+        }
+    }
+}
