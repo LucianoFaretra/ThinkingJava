2d8d815adb4e3f7ce2f69d4a60d2c6d6a2c30ee6
LucianoFaretra <lucianofaretra@gmail.com>
2017-03-03 14:49:54 +0100

    Aggiunta Es7 cap 6

---
+++ Cap_6_Es7/src/Package1/Widget.java
@@ -0,0 +1,8 @@
+package Package1;
+
+/**
+ * Creazione classe Widget come da librp
+ * @author Luciano Faretra
+ * @version 0.1
+ */
+public class Widget {}
---
+++ Cap_6_Es7/src/Package2/OtherPackage.java
@@ -0,0 +1,12 @@
+package Package2;
+
+import Package1.Widget;
+
+/**
+ * Creazione di un oggetto widged da un package differente da quello in cui è presente Widget
+ * @author Luciano Faretra
+ * @version 0.1
+ */
+public class OtherPackage {
+    Widget hello = new Widget();//Creazione oggetto di altro package avvenuta con successo
+}
